70311a22e2e972d0af4604c6008cf68134105e5c
Alfi Maulana <alfi.maulana.f@gmail.com>
2022-12-24 15:23:03 +0700

    add test for `tcp.Server`

---
+++ pkg/tcp/server_test.go
@@ -0,0 +1,33 @@
+package tcp
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestServer(t *testing.T) {
+	t.Run("NewServer", func(t *testing.T) {
+		server, err := NewServer(":50050")
+		if err != nil {
+			t.Fatalf("failed to create a new server on `:50050`: %v", err)
+		}
+		if server == nil {
+			t.Fatal("Expected `server` to be non-nil, got nil instead")
+		}
+		t.Run("Serve", func(t *testing.T) {
+			res := make(chan error)
+			go func() { res <- server.Serve() }()
+			server.server.GracefulStop()
+			err = <-res
+			if !strings.Contains(err.Error(), "the server has been stopped") {
+				t.Fatalf("failed during serving the server: %v", err)
+			}
+		})
+	})
+	t.Run("NewServerFailure", func(t *testing.T) {
+		_, err := NewServer("invalid")
+		if err == nil {
+			t.Fatal("Expected `err` to be non-nil, got nil instead")
+		}
+	})
+}
