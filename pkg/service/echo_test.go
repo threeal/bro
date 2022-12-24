e5c31bc51bd0475a9e2defead362ee785095d404
Alfi Maulana <alfi.maulana.f@gmail.com>
2022-12-24 15:54:19 +0700

    implement `tcp.Server` in `pkg/service`

--- pkg/service/echo_test.go
+++ pkg/service/echo_test.go
@@ -2,24 +2,22 @@ package service
 
 import (
 	"context"
-	"net"
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/threeal/threeal-bot/pkg/schema"
+	"github.com/threeal/threeal-bot/pkg/tcp"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
 func TestEchoServer(t *testing.T) {
-	lis, err := net.Listen("tcp", ":50050")
-	if err != nil {
-		t.Fatalf("failed to listen to ':50050': %v", err)
-	}
-	server := grpc.NewServer()
+	server, err := tcp.NewServer(":50050")
+	assert.NoError(t, err)
 	schema.RegisterEchoServer(server, &EchoServer{})
-	go func() { server.Serve(lis) }()
-	time.Sleep(100 * time.Millisecond)
+	go func() { server.Serve() }()
+	time.Sleep(30 * time.Millisecond)
 	conn, err := grpc.Dial("localhost:50050", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		t.Fatalf("failed to connect to 'localhost:50050': %v", err)
