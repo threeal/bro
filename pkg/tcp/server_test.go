2089731e7801dd513cbb0a8cae1b27098b6b1de8
Alfi Maulana <alfi.maulana.f@gmail.com>
2022-12-24 15:49:32 +0700

    use `testify/assert` in `tcp/server_test.go`

--- pkg/tcp/server_test.go
+++ pkg/tcp/server_test.go
@@ -1,33 +1,26 @@
 package tcp
 
 import (
-	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestServer(t *testing.T) {
 	t.Run("NewServer", func(t *testing.T) {
 		server, err := NewServer(":50050")
-		if err != nil {
-			t.Fatalf("failed to create a new server on `:50050`: %v", err)
-		}
-		if server == nil {
-			t.Fatal("Expected `server` to be non-nil, got nil instead")
-		}
+		assert.NoError(t, err)
+		assert.NotNil(t, server)
 		t.Run("Serve", func(t *testing.T) {
 			res := make(chan error)
 			go func() { res <- server.Serve() }()
 			server.GracefulStop()
 			err = <-res
-			if !strings.Contains(err.Error(), "the server has been stopped") {
-				t.Fatalf("failed during serving the server: %v", err)
-			}
+			assert.ErrorContains(t, err, "the server has been stopped")
 		})
 	})
 	t.Run("NewServerFailure", func(t *testing.T) {
 		_, err := NewServer("invalid")
-		if err == nil {
-			t.Fatal("Expected `err` to be non-nil, got nil instead")
-		}
+		assert.Error(t, err)
 	})
 }
