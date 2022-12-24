080e9a1668a070fd13c6bb6730f4f55742b1d9db
Alfi Maulana <alfi.maulana.f@gmail.com>
2022-12-24 16:23:32 +0700

    use `testify/require` in `tcp/server_test.go`

--- pkg/tcp/server_test.go
+++ pkg/tcp/server_test.go
@@ -3,24 +3,24 @@ package tcp
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestServer(t *testing.T) {
 	t.Run("NewServer", func(t *testing.T) {
 		server, err := NewServer(":50050")
-		assert.NoError(t, err)
-		assert.NotNil(t, server)
+		require.NoError(t, err)
+		require.NotNil(t, server)
 		t.Run("Serve", func(t *testing.T) {
 			res := make(chan error)
 			go func() { res <- server.Serve() }()
 			server.GracefulStop()
 			err = <-res
-			assert.ErrorContains(t, err, "the server has been stopped")
+			require.ErrorContains(t, err, "the server has been stopped")
 		})
 	})
 	t.Run("NewServerFailure", func(t *testing.T) {
 		_, err := NewServer("invalid")
-		assert.Error(t, err)
+		require.Error(t, err)
 	})
 }
