81205460551945d60510917745f680ab37fcba72
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-01-28 14:28:54 +0700

    add `TestMain` func for each package that have test files

---
+++ pkg/tcp/main_test.go
@@ -0,0 +1,11 @@
+package tcp
+
+import (
+	"testing"
+
+	"github.com/threeal/bro/pkg/internal"
+)
+
+func TestMain(m *testing.M) {
+	internal.RunTestWithCoverage(m, 1)
+}
---
+++ pkg/utils/main_test.go
@@ -0,0 +1,11 @@
+package utils
+
+import (
+	"testing"
+
+	"github.com/threeal/bro/pkg/internal"
+)
+
+func TestMain(m *testing.M) {
+	internal.RunTestWithCoverage(m, 1)
+}
