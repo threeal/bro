fcdd58960c3ae3c8d2c0111dbf0a5d0b4e6675b0
LordRonz <aarontanhar2000@gmail.com>
2023-01-28 11:19:19 +0700

    refactor: rewrite config_test :rocket:

--- pkg/utils/config_test.go
+++ pkg/utils/config_test.go
@@ -40,94 +40,87 @@ func (c *errorConfig) Init() error {
 	return errors.New("init error")
 }
 
-type noFieldConfig struct {
+type funcFieldConfig struct {
 	BackendAddr func() `json:"backend_addr"`
 }
 
-func (c *noFieldConfig) Read() error {
+func (c *funcFieldConfig) Read() error {
 	return errors.New("read error")
 }
 
-func (c *noFieldConfig) Write() error {
+func (c *funcFieldConfig) Write() error {
 	return errors.New("write error")
 }
 
-func (c *noFieldConfig) Init() error {
+func (c *funcFieldConfig) Init() error {
 	return errors.New("init error")
 }
 
-func TestConfigIO(t *testing.T) {
+func TestConfig(t *testing.T) {
 	filename := "ozymandias"
 	addr := ":21"
-	conf := &testConfig{&addr}
-	_, err := InitializeConfig(conf)
-	require.NoError(t, err)
-	require.Equal(t, addr, *conf.BackendAddr)
-	err = WriteConfigToFile(conf, filename)
-	require.NoError(t, err)
-	err = ReadConfigFromFile(conf, filename)
-	require.NoError(t, err)
-	err = DeleteConfig(filename)
-	require.NoError(t, err)
-}
-
-func TestConfigIOErr(t *testing.T) {
-	filename := "ozymandias"
-	addr := ":21"
-	conf := &errorConfig{&addr}
-	_, err := InitializeConfig(conf)
-	require.Error(t, err)
-	require.Equal(t, addr, *conf.BackendAddr)
-	err = WriteConfigToFile(conf, filename)
-	require.NoError(t, err)
-	err = ReadConfigFromFile(conf, filename)
-	require.NoError(t, err)
-	err = DeleteConfig(filename)
-	require.NoError(t, err)
-}
-
-func TestCreateFolder(t *testing.T) {
-	err := createFolder("banger")
-	require.NoError(t, err)
-	err = os.RemoveAll("banger")
-	require.NoError(t, err)
-}
-
-func TestConfigIOErrHomeDir(t *testing.T) {
-	os.Setenv("HOME", "/home/somerandomhomethatsnotsupposedtobepresent")
-	filename := "ozymandias"
-	addr := ":21"
-	conf := &errorConfig{&addr}
-	_, err := InitializeConfig(conf)
-	require.Error(t, err)
-	require.Equal(t, addr, *conf.BackendAddr)
-	err = WriteConfigToFile(conf, filename)
-	require.Error(t, err)
-	err = ReadConfigFromFile(conf, filename)
-	require.Error(t, err)
-	err = DeleteConfig(filename)
-	require.Error(t, err)
-}
-
-func TestConfigIONoHomeDir(t *testing.T) {
-	os.Unsetenv("HOME")
-	filename := "ozymandias"
-	addr := ":21"
-	conf := &errorConfig{&addr}
-	_, err := InitializeConfig(conf)
-	require.Error(t, err)
-	require.Equal(t, addr, *conf.BackendAddr)
-	err = WriteConfigToFile(conf, filename)
-	require.Error(t, err)
-	err = ReadConfigFromFile(conf, filename)
-	require.Error(t, err)
-	err = DeleteConfig(filename)
-	require.Error(t, err)
-	os.Setenv("HOME", "/home/somerandomhomethatsnotsupposedtobepresent")
-}
-
-func TestConfigIOMarshalError(t *testing.T) {
-	conf := &noFieldConfig{func() {}}
-	err := WriteConfigToFile(conf, "")
-	require.Error(t, err)
+	tmpDir := t.TempDir()
+	os.Setenv("HOME", tmpDir)
+	t.Run("it should successfully create config flow", func (t *testing.T) {
+		conf := &testConfig{&addr}
+		_, err := InitializeConfig(conf)
+		require.NoError(t, err)
+		require.Equal(t, addr, *conf.BackendAddr)
+		err = WriteConfigToFile(conf, filename)
+		require.NoError(t, err)
+		err = ReadConfigFromFile(conf, filename)
+		require.NoError(t, err)
+		err = DeleteConfig(filename)
+		require.NoError(t, err)
+	})
+	t.Run("it should be failed to initialize config", func (t *testing.T) {
+		conf := &errorConfig{&addr}
+		_, err := InitializeConfig(conf)
+		require.Error(t, err)
+		require.Equal(t, addr, *conf.BackendAddr)
+		err = WriteConfigToFile(conf, filename)
+		require.NoError(t, err)
+		err = ReadConfigFromFile(conf, filename)
+		require.NoError(t, err)
+		err = DeleteConfig(filename)
+		require.NoError(t, err)
+	})
+	t.Run("it should create and delete folder", func (t *testing.T) {
+		err := createFolder("banger")
+		require.NoError(t, err)
+		err = os.RemoveAll("banger")
+		require.NoError(t, err)
+	})
+	t.Run("it should error when writing config", func (t *testing.T) {
+		os.Setenv("HOME", "/home/somerandomhomethatsnotsupposedtobepresent")
+		conf := &errorConfig{&addr}
+		_, err := InitializeConfig(conf)
+		require.Error(t, err)
+		require.Equal(t, addr, *conf.BackendAddr)
+		err = WriteConfigToFile(conf, filename)
+		require.Error(t, err)
+		err = ReadConfigFromFile(conf, filename)
+		require.Error(t, err)
+		err = DeleteConfig(filename)
+		require.Error(t, err)
+	})
+	t.Run("it should error when $HOME env is unset", func (t *testing.T) {
+		os.Unsetenv("HOME")
+		conf := &errorConfig{&addr}
+		_, err := InitializeConfig(conf)
+		require.Error(t, err)
+		require.Equal(t, addr, *conf.BackendAddr)
+		err = WriteConfigToFile(conf, filename)
+		require.Error(t, err)
+		err = ReadConfigFromFile(conf, filename)
+		require.Error(t, err)
+		err = DeleteConfig(filename)
+		require.Error(t, err)
+		os.Setenv("HOME", tmpDir)
+	})
+	t.Run("it should error when marshaling a func", func (t *testing.T) {
+		conf := &funcFieldConfig{func() {}}
+		err := WriteConfigToFile(conf, "")
+		require.Error(t, err)
+	})
 }
