92cd3068799a2dad555ef006fbfc7c731039a0fe
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-02-03 03:34:15 +0000

    check valid arguments in the echo command of Bro client

--- cmd/bro/commands/echo.go
+++ cmd/bro/commands/echo.go
@@ -11,9 +11,10 @@ import (
 
 func getEchoCommand() *cobra.Command {
 	return &cobra.Command{
-		Use:   "echo [MESSAGE]",
+		Use:   "echo [messages...]",
 		Short: "Echo command",
 		Long:  `A command that outputs the strings that are passed to it as arguments.`,
+		Args: cobra.MinimumNArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
 			msg := args
 			clientCmd := cmd.Name()
