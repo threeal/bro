103404f8d8a217080bd1ba3fb2654290753d445e
LordRonz <aarontanhar2000@gmail.com>
2023-02-07 12:34:22 +0700

    feat: add status command for linux only :rocket:

--- cmd/bro-backend/commands/root.go
+++ cmd/bro-backend/commands/root.go
@@ -1,9 +1,16 @@
 package commands
 
-import "github.com/spf13/cobra"
+import (
+	"runtime"
+
+	"github.com/spf13/cobra"
+)
 
 func Execute() {
 	var rootCmd = &cobra.Command{Use: "bro-backend"}
-	rootCmd.AddCommand(getSpinCommand(), getStatusCommand())
+	rootCmd.AddCommand(getSpinCommand())
+	if (runtime.GOOS == "linux") {
+		rootCmd.AddCommand(getStatusCommand())
+	}
 	rootCmd.Execute()
 }
--- cmd/bro-backend/commands/status.go
+++ cmd/bro-backend/commands/status.go
@@ -2,14 +2,9 @@ package commands
 
 import (
 	"fmt"
-	"log"
 	"os/exec"
-	"runtime"
 
 	"github.com/spf13/cobra"
-	"github.com/threeal/bro/cmd/bro-backend/config"
-	backendConfig "github.com/threeal/bro/cmd/bro-backend/config"
-	"github.com/threeal/bro/pkg/utils"
 )
 
 func getStatusCommand() *cobra.Command {
@@ -19,15 +14,6 @@ func getStatusCommand() *cobra.Command {
 		Long:  `A command to check Bro backend if config exist and the service is running..`,
 		Args:  cobra.NoArgs,
 		Run: func(cmd *cobra.Command, args []string) {
-			if runtime.GOOS != "linux" {
-				log.Fatalf("This command only available on Linux.")
-			}
-			config := &config.Config{}
-			if err := utils.ReadConfigFromFile(config, backendConfig.CONFIG_FILENAME); err == nil {
-				fmt.Printf("Bro config file found!, listen addr = %s\n", config.ListenAddr)
-			} else {
-				fmt.Println("Bro config file not found, consider running the spin command")
-			}
 			broService := exec.Command("systemctl", "check", "bro")
 			out, _ := broService.CombinedOutput()
 			fmt.Printf("Bro service status is: %s\n", string(out))
