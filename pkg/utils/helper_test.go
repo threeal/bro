0555c53d36a9a370eec7cd3b1130114c9e84fbab
LordRonz <aarontanhar2000@gmail.com>
2023-02-08 11:50:07 +0700

    fix(attempt): failed coverage utils :rocket:

--- pkg/utils/helper_test.go
+++ pkg/utils/helper_test.go
@@ -13,12 +13,21 @@ type helperConfig struct {
 	AnotherField string `json:","`
 }
 
+type emptyConf struct {}
+
 func TestHelper(t *testing.T) {
-	require.True(t, StringInSlice("a", []string{"a", "b"}))
-	require.False(t, StringInSlice("c", []string{"a", "b"}))
+	t.Run("StringInSlice should output correct boolean", func(t *testing.T) {
+		require.True(t, StringInSlice("a", []string{"a", "b"}))
+		require.False(t, StringInSlice("c", []string{"a", "b"}))
+	})
+
 	t.Run("GetJSONFields should return slices of string with length of 3", func(t *testing.T) {
 		res := GetJSONFields(helperConfig{"yes", "yes", "yes", "yes"})
 		require.Len(t, res, 3)
+	})
+	t.Run("GetJSONFields should return slices with length of 0", func(t *testing.T) {
+		res := GetJSONFields(emptyConf{})
+		require.Len(t, res, 0)
 	})
 
 	t.Run("GetStructValueByJSON should return value of yes", func(t *testing.T) {
@@ -35,5 +44,10 @@ func TestHelper(t *testing.T) {
 		c := &helperConfig{"yes", "yes", "yes", "yes"}
 		SetStructValueByJSON(c, "backend_addr", "banger")
 		require.Equal(t, c.BackendAddr, "banger")
+	})
+	t.Run("SetStructValueByJSON should set the field correctly when key is not found", func(t *testing.T) {
+		c := &helperConfig{"yes", "yes", "yes", "yes"}
+		SetStructValueByJSON(c, "yessr", "banger")
+		require.Equal(t, c.BackendAddr, "yes")
 	})
 }
